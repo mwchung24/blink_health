3eed2a6405a6d1c7f4ab659f0f053f929e341914
Martin Chung <mwchung24@gmail.com>
2024-06-20 14:21:50 -0400

    only have 2 paths

--- blink-health/src/App.tsx
+++ blink-health/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import {QueryClientProvider, QueryClient} from '@tanstack/react-query';
 import {SearchPage} from './containers/SearchPage';
 import {DrugPage} from './containers/DrugPage';
@@ -11,9 +11,9 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SearchPage />} />
           <Route path="/drugs/search" element={<SearchPage />} />
           <Route path="/drugs/:drugName" element={<DrugPage />} />
+          <Route path="*" element={<Navigate to="/drugs/search" />} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
