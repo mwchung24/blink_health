254ae9ee0d086b11d8341cfcbcfd8c7f0388712f
Martin Chung <mwchung24@gmail.com>
2024-06-20 14:35:04 -0400

    add API calls

---
+++ blink-health/src/data-provider/data-service.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+export const getDrugs = async (search: string) => {
+  const response = await axios.get(`https://rxnav.nlm.nih.gov/REST/drugs.json?name=${search}`);
+
+  return response.data;
+};
+
+export const getSpellingSuggestions = async (search: string) => {
+  const response = await axios.get(
+    `https://rxnav.nlm.nih.gov/REST/spellingsuggestions.json?name=${search}`
+  );
+
+  return response.data;
+};
