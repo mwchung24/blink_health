2d9232d2e6d36689734b6ba0d3e484dade199e40
Martin Chung <mwchung24@gmail.com>
2024-06-20 14:35:21 -0400

    add primereact styles

--- blink-health/src/index.tsx
+++ blink-health/src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'primereact/resources/themes/soho-light/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
