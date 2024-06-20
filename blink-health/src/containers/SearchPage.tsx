8364404f8075f0d227be3e333fe7e6d67bf60ef0
Martin Chung <mwchung24@gmail.com>
2024-06-20 15:18:41 -0400

    search page

--- blink-health/src/containers/SearchPage.tsx
+++ blink-health/src/containers/SearchPage.tsx
@@ -1,3 +1,40 @@
+import {useState} from 'react';
+import {useQuery} from '@tanstack/react-query';
+import {SearchBar} from '../components/SearchBar';
+import {getDrugs} from '../data-provider/data-service';
+import {TDrug} from '../utils/types';
+import {Header} from '../components/Header';
+import styles from './SearchPage.module.css';
+
 export const SearchPage = () => {
-  return <div>Search Page</div>;
+  const [drugName, setDrugName] = useState<string>('');
+
+  const fetchDrugsQuery = useQuery({
+    queryKey: ['drug' + drugName],
+    queryFn: () => getDrugs(drugName),
+    enabled: !!drugName,
+  });
+  const handleOnChange = (drugName: string): void => {
+    setDrugName(drugName);
+    fetchDrugsQuery.refetch();
+  };
+
+  const drugs = fetchDrugsQuery?.data?.drugGroup?.conceptGroup
+    ?.map((drugGroup: {tty: string; conceptProperties: TDrug[]}) => {
+      return drugGroup?.conceptProperties;
+    })
+    ?.flat()
+    .map((drug: TDrug) => {
+      return drug?.name;
+    });
+
+  return (
+    <div className={styles.searchPage}>
+      <Header title="Drug Search" />
+      <div className={styles.searchBarWrapper}>
+        <div className={styles.label}>Search For Drugs!</div>
+        <SearchBar handleOnChange={handleOnChange} drugs={drugs} />
+      </div>
+    </div>
+  );
 };
