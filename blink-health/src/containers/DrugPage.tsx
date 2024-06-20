1bac936aba623732c46e490b9031edd670a061c6
Martin Chung <mwchung24@gmail.com>
2024-06-20 14:35:37 -0400

    create drug page

--- blink-health/src/containers/DrugPage.tsx
+++ blink-health/src/containers/DrugPage.tsx
@@ -1,3 +1,31 @@
+import {useQuery} from '@tanstack/react-query';
+import {getDrugs} from '../data-provider/data-service';
+import {useParams, useLocation} from 'react-router-dom';
+import {TDrug} from '../utils/types';
+
 export const DrugPage = () => {
+  let {drugName} = useParams();
+  const location = useLocation();
+
+  console.log('drugName', drugName);
+  console.log('location', location.state);
+
+  const fetchDrugsQuery = useQuery({
+    queryKey: ['drug' + drugName],
+    queryFn: () => getDrugs(drugName || ''),
+    enabled: !!drugName,
+  });
+
+  const drug = fetchDrugsQuery?.data?.drugGroup?.conceptGroup
+    ?.map((drugGroup: {tty: string; conceptProperties: TDrug[]}) => {
+      return drugGroup?.conceptProperties;
+    })
+    ?.flat()
+    .filter((drug: TDrug) => {
+      return drug.name === location.state.name;
+    })[0];
+
+  console.log('drug', drug);
+
   return <div>Drug Page</div>;
 };
