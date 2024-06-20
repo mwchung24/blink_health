58e115e18016d6884788a6c2e669b746118775e0
Martin Chung <mwchung24@gmail.com>
2024-06-20 15:24:37 -0400

    search bar

---
+++ blink-health/src/components/SearchBar.tsx
@@ -0,0 +1,68 @@
+import {useState, useRef} from 'react';
+import {
+  AutoComplete,
+  AutoCompleteChangeEvent,
+  AutoCompleteSelectEvent,
+} from 'primereact/autocomplete';
+import {Button} from 'primereact/button';
+import {useNavigate} from 'react-router-dom';
+import styles from './SearchBar.module.css';
+
+export const SearchBar = ({
+  handleOnChange,
+  drugs,
+}: {
+  handleOnChange: (drugName: string) => void;
+  drugs: any[];
+}) => {
+  const [drugName, setDrugName] = useState<string>('');
+  const navigate = useNavigate();
+
+  const ref = useRef(null);
+
+  const onChange = (e: AutoCompleteChangeEvent) => {
+    setDrugName(e.value);
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter') {
+      handleOnChange(drugName);
+      // @ts-ignore - Property 'show' does not exist on type 'never'.
+      ref?.current?.show();
+    }
+  };
+
+  const handleSelect = (e: AutoCompleteSelectEvent) => {
+    console.log('onSelect', drugName);
+    console.log('onSelect', e);
+    // @ts-ignore
+    navigate(`/drugs/${drugName}`, {state: {name: e.value}});
+  };
+
+  return (
+    <div>
+      <AutoComplete
+        inputClassName={styles.autocompleteInput}
+        panelClassName={styles.autocompletePanel}
+        onChange={onChange}
+        value={drugName}
+        onKeyDown={handleKeyDown}
+        suggestions={drugs}
+        completeMethod={() => {}}
+        ref={ref}
+        // @ts-ignore - Property 'show' does not exist on type 'never'.
+        onFocus={() => ref?.current?.show()}
+        onSelect={handleSelect}
+      />
+      <Button
+        icon="pi pi-search"
+        aria-label="search"
+        onClick={() => {
+          handleOnChange(drugName);
+          // @ts-ignore - Property 'show' does not exist on type 'never'.
+          ref?.current?.show();
+        }}
+      />
+    </div>
+  );
+};
