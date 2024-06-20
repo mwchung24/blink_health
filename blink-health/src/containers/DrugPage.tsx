bf62d23783784f867acd3ac7084b4994b46eadbd
Martin Chung <mwchung24@gmail.com>
2024-06-20 15:13:19 -0400

    drug page

--- blink-health/src/containers/DrugPage.tsx
+++ blink-health/src/containers/DrugPage.tsx
@@ -1,7 +1,10 @@
 import {useQuery} from '@tanstack/react-query';
-import {getDrugs} from '../data-provider/data-service';
+import {getDrugs, getNDCs} from '../data-provider/data-service';
 import {useParams, useLocation} from 'react-router-dom';
 import {TDrug} from '../utils/types';
+import {Header} from '../components/Header';
+import {DrugInfo} from '../components/DrugInfo';
+import {AssociatedNDCs} from '../components/AssociatedNDCs';
 
 export const DrugPage = () => {
   let {drugName} = useParams();
@@ -27,5 +30,19 @@ export const DrugPage = () => {
 
   console.log('drug', drug);
 
-  return <div>Drug Page</div>;
+  const fetchNDCs = useQuery({
+    queryKey: ['ndcs' + drug?.rxcui],
+    queryFn: () => getNDCs(drug?.rxcui),
+    enabled: !!drug?.rxcui,
+  });
+
+  console.log('fetchNDCs', fetchNDCs);
+
+  return (
+    <div>
+      <Header title="Drug Details" />
+      <DrugInfo drug={drug} />
+      <AssociatedNDCs ndcs={fetchNDCs?.data?.ndcGroup?.ndcList?.ndc} />
+    </div>
+  );
 };
